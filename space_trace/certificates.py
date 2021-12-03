7e843294ea166f91db5077435323c51e1b157a73
flofriday <flohacksfriday@gmail.com>
2021-12-04 00:18:42 +0100

    Clean up certificate checking code

--- space_trace/certificates.py
+++ space_trace/certificates.py
@@ -20,41 +20,6 @@ from space_trace.models import User
 # Source: https://github.com/ehn-dcc-development/ehn-dcc-schema/blob/release/1.3.0/valuesets/disease-agent-targeted.json
 COVID_19_ID = "840539006"
 
-# TODO: we should get this from the Austria API
-# Source: https://github.com/ehn-dcc-development/ehn-dcc-schema/blob/release/1.3.0/valuesets/vaccine-medicinal-product.json
-VACCINE_MANUFACTURERS = {
-    "ORG-100001699": "AstraZeneca AB",
-    "ORG-100030215": "Biontech Manufacturing GmbH",
-    "ORG-100001417": "Janssen-Cilag International",
-    "ORG-100031184": "Moderna Biotech Spain S.L.",
-    "ORG-100006270": "Curevac AG",
-    "ORG-100013793": "CanSino Biologics",
-    "ORG-100020693": "China Sinopharm International Corp. - Beijing location",
-    "ORG-100010771": "Sinopharm Weiqida Europe Pharmaceutical s.r.o. - Prague location",
-    "ORG-100024420": "Sinopharm Zhijun (Shenzhen) Pharmaceutical Co. Ltd. - Shenzhen location",
-    "ORG-100032020": "Novavax CZ AS",
-    "Gamaleya-Research-Institute": "Gamaleya Research Institute",
-    "Vector-Institute": "Vector Institute",
-    "Sinovac-Biotech": "Sinovac Biotech",
-    "Bharat-Biotech": "Bharat Biotech",
-}
-
-# Source: https://github.com/ehn-dcc-development/ehn-dcc-schema/blob/release/1.3.0/valuesets/vaccine-medicinal-product.json
-VACCINE_PRODUCTS = {
-    "EU/1/20/1528": "Comirnaty",
-    "EU/1/20/1507": "COVID-19 Vaccine Moderna",
-    "EU/1/21/1529": "Vaxzevria",
-    "EU/1/20/1525": "COVID-19 Vaccine Janssen",
-    "CVnCoV": "CVnCoV",
-    "Sputnik-V": "Sputnik-V",
-    "Convidecia": "Convidecia",
-    "EpiVacCorona": "EpiVacCorona",
-    "BBIBP-CorV": "BBIBP-CorV",
-    "Inactivated-SARS-CoV-2-Vero-Cell": "Inactivated SARS-CoV-2 (Vero Cell)",
-    "CoronaVac": "CoronaVac",
-    "Covaxin": "Covaxin (also known as BBV152 A, B, C)",
-}
-
 
 def calc_vacinated_till(data) -> date:
     hcert = data[-260][1]["v"][0]
