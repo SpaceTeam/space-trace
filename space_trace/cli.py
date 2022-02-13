3f5f8975655ccd4c3f73236299057c08778e299e
flofriday <flohacksfriday@gmail.com>
2022-02-13 22:03:47 +0100

    Improve statistics UX for racing team The racing team has now their member names over the space users which makes sense as they will probably only be interested in their own members.

--- space_trace/cli.py
+++ space_trace/cli.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import click
 from space_trace import app, db
 from space_trace.models import User, Visit
@@ -30,3 +31,13 @@ def delete_debug_visits():
     db.session.query(Visit).filter(Visit.user == user.id).delete()
     db.session.commit()
     print("✅ Deleted debug visits")
+
+
+@app.cli.command("insert-visits")
+def insert_visits():
+    for i in range(16):
+        visit = Visit(datetime.now(), i)
+        db.session.add(visit)
+
+    db.session.commit()
+    print("✅ Inserted 16 visits")
